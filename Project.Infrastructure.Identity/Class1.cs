c5329b353e0f67e04f7a7ce484e66b76553aa8c0
bahtitm <bahti1986@gmail.com>
2021-08-07 00:17:23 +0500

    Add project files.

---
+++ Project.Infrastructure.Identity/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Project.Infrastructure.Identity
+{
+    public class Class1
+    {
+    }
+}
---
+++ Project.Infrastructure.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Project.Infrastructure.Persistence
+{
+    public class Class1
+    {
+    }
+}
